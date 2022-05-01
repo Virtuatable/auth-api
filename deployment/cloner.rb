08bd7e8b62d1210502e4e2da880be6ec72dc82c1
Vincent Courtois <vincent.courtois@coddity.com>
2022-05-01 13:36:09 +0200

    Change the directory where the frontend is copied (#6)

--- deployment/cloner.rb
+++ deployment/cloner.rb
@@ -15,9 +15,10 @@ object = bucket.object('auth-gui/current') # 'auth-gui/1650897348')
 
 version = object.get.body.read.to_s
 
+puts "[COPYING auth-gui/#{version}]"
 bucket.objects(prefix: "auth-gui/#{version}").each do |obj|
   key = obj.key.clone
-  key["auth-gui/#{version}"] = 'public'
+  key["auth-gui/#{version}"] = 'public/auth'
   File.write(key, obj.get.body.read.to_s)
   puts "#{obj.key} -> #{key}"
 end
